b30e1ae1e760a5994181eb60491f83c99dd9ff19
Amari W <amwalker@claflin.edu>
2026-02-05 10:24:30 -0500

    Add files via upload

---
+++ Test Descriptive Measures (Practice).R
@@ -0,0 +1,19 @@
+# Author: Walker, Date: 02/05/2026, Purpose: Test Descriptive Measures (Practice)
+
+# Create variable called "variablenew" holding 100 values from 100 to 200.
+# variablenew <-c(100:200)
+
+# Calculate mean on variablenew for points 150 to 200
+# mean(subset(variablenew, variablenew >= 150 & variablenew <= 200))
+# [1] 175
+
+# Calculate median on variablenew
+# median(variablenew)
+#[1] 150
+
+# Calculate mean on variablenew for points 100 to 150
+# mean(subset(variablenew, variablenew >= 100 & variablenew <= 150))
+# [1] 125
+
+
+Save the outputs after each function in this file, upload this file on your GitHub repository to get full points.
